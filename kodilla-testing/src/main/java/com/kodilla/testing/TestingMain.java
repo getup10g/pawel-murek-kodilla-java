5f640d495f483ed0f755e0c73751ac6520baf74e
getup10g <getup@10g.pl>
2017-12-06 14:47:44 +0000

    calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int add = calculator.add(5,5);
+        int substract = calculator.substract(5,5);
+        if(add==10&&substract==0){
+            System.out.println("calculator test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
-
